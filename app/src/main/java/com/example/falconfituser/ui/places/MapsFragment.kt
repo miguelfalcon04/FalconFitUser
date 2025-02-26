4da079fd2506362bbf62173f892e386e8e4b8d3a
Miguel <falconmiguel.2004@gmail.com>
2025-02-26 19:19:39 +0100

    Location permissions added

--- app/src/main/java/com/example/falconfituser/ui/places/MapsFragment.kt
+++ app/src/main/java/com/example/falconfituser/ui/places/MapsFragment.kt
@@ -1,5 +1,8 @@
 package com.example.falconfituser.ui.places
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
 import androidx.fragment.app.Fragment
 import android.os.Bundle
 import android.util.Log
@@ -7,11 +10,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import com.example.falconfituser.R
 import com.example.falconfituser.databinding.FragmentMapsBinding
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
-import com.example.falconfituser.data.api.places.PlaceRaw
+import androidx.navigation.fragment.findNavController
 import com.example.falconfituser.data.places.Places
 import com.google.android.gms.maps.CameraUpdateFactory
 import kotlinx.coroutines.launch
@@ -23,11 +28,30 @@ import com.google.android.gms.maps.model.MarkerOptions
 import dagger.hilt.android.AndroidEntryPoint
 import retrofit2.HttpException
 
+private var PERMISSIONS_REQUIRED = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION,
+                                           Manifest.permission.ACCESS_COARSE_LOCATION)
 @AndroidEntryPoint
 class MapsFragment : Fragment() {
     private lateinit var binding: FragmentMapsBinding
     private val viewModel: PlacesListViewModel by viewModels()
     private lateinit var map: GoogleMap
+
+    val contract = ActivityResultContracts.RequestMultiplePermissions()
+
+    val launcher = registerForActivityResult(contract){
+        permissions ->
+        var granted = true
+        permissions.entries.forEach{
+            permission ->
+                if(permission.key in PERMISSIONS_REQUIRED && !permission.value){
+                    granted = false
+                }
+        }
+
+        if(!granted){
+            Toast.makeText(requireContext(), "No tiene permisos de localizacion", Toast.LENGTH_LONG).show()
+        }
+    }
 
     private val callback = OnMapReadyCallback { googleMap ->
         map = googleMap
@@ -55,20 +79,24 @@ class MapsFragment : Fragment() {
 
     private fun printPlaces(){
         lifecycleScope.launch {
-            try {
-                val places = viewModel.loadPlaces()
-                if (places.isNotEmpty()){
-                    for (place in places){
-                        addMarker(place)
+            if (hasLocationPermissions(requireContext())){
+                try {
+                    val places = viewModel.loadPlaces()
+                    if (places.isNotEmpty()){
+                        for (place in places){
+                            addMarker(place)
+                        }
                     }
-                }
 
-            } catch (e: HttpException) {
-                Log.e("MapsFragment", "Error HTTP: ${e.message}")
-                Toast.makeText(binding.root.context, "Error al cargar sesiones", Toast.LENGTH_SHORT).show()
-            } catch (e: Exception) {
-                Log.e("MapsFragment", "Error inesperado: ${e.message}")
-                Toast.makeText(binding.root.context, "Error inesperado", Toast.LENGTH_SHORT).show()
+                } catch (e: HttpException) {
+                    Log.e("MapsFragment", "Error HTTP: ${e.message}")
+                    Toast.makeText(binding.root.context, "Error al cargar sesiones", Toast.LENGTH_SHORT).show()
+                } catch (e: Exception) {
+                    Log.e("MapsFragment", "Error inesperado: ${e.message}")
+                    Toast.makeText(binding.root.context, "Error inesperado", Toast.LENGTH_SHORT).show()
+                }
+            }else{
+                launcher.launch(PERMISSIONS_REQUIRED)
             }
         }
     }
@@ -86,6 +114,14 @@ class MapsFragment : Fragment() {
                     .title(place)
             )
             map.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 12f))
+        }
+    }
+
+    // Comprobar si tenemos permiso
+    private fun hasLocationPermissions(context: Context):Boolean{
+        return PERMISSIONS_REQUIRED.all{
+                permission ->
+            ContextCompat.checkSelfPermission(context, permission) == PackageManager.PERMISSION_GRANTED
         }
     }
 }
